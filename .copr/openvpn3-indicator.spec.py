ffb8cd1bf7ff264a0dd07982e99de17227f6f6c1
Grzegorz Gutowski <grzegorz.gutowski@uj.edu.pl>
2024-02-09 21:52:05 +0100

    Prepare RPM distribution using copr

---
+++ .copr/openvpn3-indicator.spec.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+# vim:ts=4:sts=4:sw=4:expandtab
+
+#
+# openvpn3-indicator - Simple GTK indicator GUI for OpenVPN3.
+# Copyright (C) 2024 Grzegorz Gutowski <grzegorz.gutowski@uj.edu.pl>
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License,
+# or any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.
+# If not, see <https://www.gnu.org/licenses/>.
+#
+
+import os
+import pathlib
+import re
+import subprocess
+
+os.environ['TZ'] = 'UTC'
+os.environ['LC_ALL'] = 'en_US.UTF-8'
+
+subprocess.run(['git', 'config', '--global', '--add', 'safe.directory', os.getcwd()])
+
+timestamp_run = subprocess.run(['git', 'log', '-n', '1', '--format=format:%cd', '--date=format-local:%Y%m%d%H%M%S'], capture_output=True)
+TIMESTAMP = str(timestamp_run.stdout, 'utf-8').strip()
+
+icon_path = pathlib.Path('../share/icons')
+ICON_THEMES = [ str(p.relative_to(icon_path)) for p in icon_path.glob('*') if p.is_dir() ]
+ICONS = [ str(p.relative_to(icon_path.parent)) for p in icon_path.glob('**') if p.is_file() ]
+
+mime_path = pathlib.Path('../share/mime')
+MIMES = [ str(p.relative_to(mime_path.parent)) for p in icon_path.glob('**') if p.is_file() ]
+
+app_path = pathlib.Path('../share/applications')
+APPS = [ str(p.relative_to(app_path.parent)) for p in icon_path.glob('**') if p.is_file() ]
+
+NAME      = 'openvpn3-inidicator'
+VERSION   = f'0.1.{TIMESTAMP}'
+RELEASE   = '1' #'1%{?dist}'
+SUMMARY   = 'Simple GTK indicator GUI for OpenVPN3'
+DESCRIPTION = '''
+This project adds a simple GTK indicator application that can be used to control OpenVPN3 tunnels.
+It is based on D-Bus interface provided by OpenVPN3 Linux client and does not require elevated privileges to use.
+It should be considered as a temporary work-around until Network Manager implements support for OpenVPN 3, or OpenVPN provides a graphical interface for Linux users.
+'''
+LICENSE   = 'AGPL-3.0'
+URL       = 'https://github.com/grzegorz-gutowski/openvpn3-indicator'
+BUILDARCH = 'noarch'
+BUILDREQUIRES = ' '.join(sorted([
+        'desktop-file-utils',
+    ]))
+REQUIRES  = ' '.join(sorted([
+        'openvpn3',
+        'python3-dbus',
+        'python3-secretstorage',
+        'gnome-shell-extension-appindicator',
+    ]))
+
+SOURCES = [
+    'openvpn3-indicator',
+    'Makefile',
+    'LICENSE',
+    'README.md',
+    'share',
+]
+SOURCECODE = f'openvpn3-indicator-{VERSION}.tar.gz'
+source_run = subprocess.run(['tar', '--create', '--file', SOURCECODE, '--directory', '..'] + SOURCES)
+
+PREP = '\n'.join([
+        '%setup'
+    ])
+
+BUILD = '\n'.join([
+        'make DESTDIR=%{buildroot} BINDIR=%{_bindir} DATADIR=%{_datadir} all',
+    ])
+
+INSTALL = '\n'.join([
+        'make DESTDIR=%{buildroot} BINDIR=%{_bindir} DATADIR=%{_datadir} install',
+    ])
+
+POST = '\n'.join([
+        'touch --no-create %{_datadir}/icons/'+theme+' || :' for theme in ICON_THEMES
+    ])
+
+POSTTRANS = '\n'.join([
+        'update-desktop-database %{_datadir}/applications || :',
+        'update-mime-database %{_datadir}/mime || :',
+    ] + [
+        'gtk-update-icon-cache --silent %{_datadir}/icons/'+theme+' || :' for theme in ICON_THEMES
+    ])
+
+POSTUN = '\n'.join([
+        'update-desktop-database %{_datadir}/applications || :',
+        'update-mime-database %{_datadir}/mime || :'
+    ] + [
+        'touch --no-create %{_datadir}/icons/'+theme+' || :' for theme in ICON_THEMES
+    ] + [
+        'gtk-update-icon-cache --silent %{_datadir}/icons/'+theme+' || :' for theme in ICON_THEMES
+    ])
+
+
+changelog_run = subprocess.run(['git', 'log', '-n', '1', '--format=format:* %cd %an <%ae> - '+VERSION+'-'+re.sub(r'%','%%',RELEASE)+'%n- %s%b%n', '--date=format-local:%a %b %d %Y'], capture_output=True)
+CHANGELOG = str(changelog_run.stdout, 'utf-8').strip()
+
+print(f'''
+Name: {NAME}
+Version: {VERSION}
+Release: {RELEASE}
+Summary: {SUMMARY}
+License: {LICENSE}
+URL: {URL}
+BuildArch: {BUILDARCH}
+BuildRequires: {BUILDREQUIRES}
+Requires: {REQUIRES}
+Source: {SOURCECODE}
+
+%description
+
+{DESCRIPTION}
+
+%prep
+
+{PREP}
+
+%build
+
+{BUILD}
+
+%install
+
+{INSTALL}
+
+%post
+
+{POST}
+
+%posttrans
+
+{POSTTRANS}
+
+%postun
+
+{POSTUN}
+
+%changelog
+
+{CHANGELOG}
+
+''')
--- Makefile
+++ Makefile
@@ -1,19 +1,24 @@
 PROGRAM := openvpn3-indicator
 PREFIX ?= /usr/local
+BINDIR ?= $(PREFIX)/bin
+DATADIR ?= $(PREFIX)/share
 AUTOSTART ?= /etc/xdg/autostart
 
 SHARES := $(shell find share -type f -not -iname "*.[1-8]" -not -iname "*.desktop" -not -iname "*.bak" -not -iname "*.swp")
+SHARES := $(patsubst share/%,%,$(SHARES))
 MANS := $(shell find share -type f -iname "*.[1-8]")
+MANS := $(patsubst share/%,%,$(MANS))
 APPLICATIONS := $(shell find share -type f -iname "*.desktop")
+APPLICATIONS := $(patsubst share/%,%,$(APPLICATIONS))
 
-INSTALL_SHARES := $(patsubst %,$(PREFIX)/%,$(SHARES))
-INSTALL_MANS := $(patsubst %,$(PREFIX)/%.gz,$(MANS))
-INSTALL_APPLICATIONS := $(patsubst %,$(PREFIX)/%,$(APPLICATIONS))
-INSTALL_AUTOSTART := $(AUTOSTART)/$(PROGRAM).desktop
+INSTALL_SHARES := $(patsubst %,$(DESTDIR)$(DATADIR)/%,$(SHARES))
+INSTALL_MANS := $(patsubst %,$(DESTDIR)$(DATADIR)/%.gz,$(MANS))
+INSTALL_APPLICATIONS := $(patsubst %,$(DESTDIR)$(DATADIR)/%,$(APPLICATIONS))
+INSTALL_AUTOSTART := $(DESTDIR)$(AUTOSTART)/$(PROGRAM).desktop
 
-DEVEL_SHARES := $(patsubst %,$(HOME)/.local/%,$(SHARES))
+DEVEL_SHARES := $(patsubst %,$(HOME)/.local/share/%,$(SHARES))
 #DEVEL_MANS := 
-DEVEL_APPLICATIONS := $(patsubst %,$(HOME)/.local/%,$(APPLICATIONS))
+DEVEL_APPLICATIONS := $(patsubst %,$(HOME)/.local/share/%,$(APPLICATIONS))
 DEVEL_AUTOSTART := $(HOME)/.config/autostart/$(PROGRAM).desktop
 
 .PHONY: default
@@ -28,40 +33,47 @@ default:
 	@echo "Use  ***  sudo make uninstall  ***  or  ***  make indevel  ***  to uninstall $(PROGRAM)."
 	@echo
 
-.PHONY: install
-install: $(PREFIX)/bin/$(PROGRAM) $(INSTALL_SHARES) $(INSTALL_MANS) $(INSTALL_AUTOSTART)
-	update-desktop-database $(PREFIX)/share/applications
-	update-mime-database $(PREFIX)/share/mime
-	gtk-update-icon-cache -f -t $(PREFIX)/share/icons/*
+.PHONY: all
+all:
 
-$(PREFIX)/bin/$(PROGRAM) : $(PROGRAM)
+.PHONY: package
+package: $(DESTDIR)$(BINDIR)/$(PROGRAM) $(INSTALL_SHARES) $(INSTALL_MANS) $(INSTALL_AUTOSTART)
+
+
+.PHONY: install
+install: package
+	update-desktop-database $(DESTDIR)$(DATADIR)/applications
+	update-mime-database $(DESTDIR)$(DATADIR)/mime
+	gtk-update-icon-cache -f -t $(DESTDIR)$(DATADIR)/icons/*
+
+$(DESTDIR)$(BINDIR)/$(PROGRAM) : $(PROGRAM)
 	@install --directory $(dir $@)
 	install --mode 0755 $< $@
 
-$(INSTALL_SHARES): $(PREFIX)/% : %
+$(INSTALL_SHARES): $(DESTDIR)$(DATADIR)/% : share/%
 	@install --directory $(dir $@)
 	install --mode 0644 $< $@
 
-$(INSTALL_MANS): $(PREFIX)/%.gz : %
+$(INSTALL_MANS): $(DESTDIR)$(DATADIR)/%.gz : share/%
 	@install --directory $(dir $@)
 	gzip --stdout $< > $@
 	@chmod 0644 $@
 
-$(INSTALL_APPLICATIONS): $(PREFIX)/% : %
+$(INSTALL_APPLICATIONS): $(DESTDIR)$(DATADIR)/% : share/%
 	@install --directory $(dir $@)
-	sed -E -e "s|/usr/|$(PREFIX)/|g" $< > $@
+	sed -E -e "s|/usr/bin|$(BINDIR)/|g" $< > $@
 	@chmod 0644 $@
 
-$(INSTALL_AUTOSTART) : $(PREFIX)/share/applications/$(PROGRAM).desktop
+$(INSTALL_AUTOSTART) : $(DESTDIR)$(DATADIR)/applications/$(PROGRAM).desktop
 	@install --directory $(dir $@)
-	ln --force --symbolic $< $@
+	install --mode 0644 $< $@
 
 .PHONY: uninstall
 uninstall:
-	rm -f $(PREFIX)/bin/$(PROGRAM) $(INSTALL_SHARES) $(INSTALL_MANS) $(INSTALL_APPLICATIONS) $(INSTALL_AUTOSTART)
-	update-desktop-database $(PREFIX)/share/applications
-	update-mime-database $(PREFIX)/share/mime
-	gtk-update-icon-cache -f -t $(PREFIX)/share/icons/*
+	rm -f $(DESTDIR)$(BINDIR)/$(PROGRAM) $(INSTALL_SHARES) $(INSTALL_MANS) $(INSTALL_APPLICATIONS) $(INSTALL_AUTOSTART)
+	update-desktop-database $(DESTDIR)$(DATADIR)/applications
+	update-mime-database $(DESTDIR)$(DATADIR)/mime
+	gtk-update-icon-cache -f -t $(DESTDIR)$(DATADIR)/icons/*
 
 .PHONY: devel
 devel: $(HOME)/.local/bin/$(PROGRAM) $(DEVEL_SHARES) $(DEVEL_APPLICATIONS) $(DEVEL_AUTOSTART)
@@ -73,11 +85,11 @@ $(HOME)/.local/bin/$(PROGRAM) : $(PROGRAM)
 	@install --directory $(dir $@)
 	ln --force --symbolic $(abspath $<) $@
 
-$(DEVEL_SHARES): $(HOME)/.local/% : %
+$(DEVEL_SHARES): $(HOME)/.local/share/% : share/%
 	@install --directory $(dir $@)
 	ln --force --symbolic $(abspath $<) $@
 
-$(DEVEL_APPLICATIONS): $(HOME)/.local/% : %
+$(DEVEL_APPLICATIONS): $(HOME)/.local/share/% : share/%
 	@install --directory $(dir $@)
 	sed -E -e "s|/usr/|$(HOME)/.local/|g" $< > $@
 	@chmod 0644 $@
