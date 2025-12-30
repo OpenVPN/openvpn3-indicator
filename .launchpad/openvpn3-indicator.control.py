e06ce6863ac33b9ffb8dd3b4f3c37a1304f0c256
Grzegorz Gutowski <grzegorz.gutowski@uj.edu.pl>
2025-12-30 17:04:30 +0100

    ubuntu 26.04

--- .launchpad/openvpn3-indicator.control.py
+++ .launchpad/openvpn3-indicator.control.py
@@ -34,7 +34,7 @@ scriptdir=pathlib.Path(args.scriptdir)
 outdir=pathlib.Path(args.outdir)
 gitdir=scriptdir.parent
 
-DISTROS = ['focal','jammy','noble','plucky','questing']
+DISTROS = ['focal','jammy','noble','plucky','questing','resolute']
 
 
 os.environ['TZ'] = 'UTC'
